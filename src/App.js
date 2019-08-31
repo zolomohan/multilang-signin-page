ecbf35bcdd9f0c9ae9c0c295bf48beb90dcd5cd4
ZoloMohan <mohanzolomon@gmail.com>
2019-08-31 23:44:45 +0530

    Convert App from Class -> Functional Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
 import Page from './Page';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 
-class App extends Component {
-	render() {
-		return (
-			<ThemeProvider>
-				<LanguageProvider>
-					<Page>
-						<Navbar />
-						<Form />
-					</Page>
-				</LanguageProvider>
-			</ThemeProvider>
-		);
-	}
+export default function App() {
+	return (
+		<ThemeProvider>
+			<LanguageProvider>
+				<Page>
+					<Navbar />
+					<Form />
+				</Page>
+			</LanguageProvider>
+		</ThemeProvider>
+	);
 }
-
-export default App;
