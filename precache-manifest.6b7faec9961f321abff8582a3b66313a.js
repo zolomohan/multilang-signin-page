3f79be82bf44b611e2ec842711f3d3f1960148e4
ZoloMohan <mohanzolomon@gmail.com>
2019-09-01 00:18:34 +0530

    Updates

---
+++ precache-manifest.6b7faec9961f321abff8582a3b66313a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79a9b736a18413755555e226b7b99a3",
+    "url": "/multilang-signin-page/index.html"
+  },
+  {
+    "revision": "af1c21d1538f2dfb9166",
+    "url": "/multilang-signin-page/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "229dc5fbcbd095de00a9",
+    "url": "/multilang-signin-page/static/js/2.15c552e0.chunk.js"
+  },
+  {
+    "revision": "af1c21d1538f2dfb9166",
+    "url": "/multilang-signin-page/static/js/main.90630f7f.chunk.js"
+  },
+  {
+    "revision": "01db0276afb40ebe92c1",
+    "url": "/multilang-signin-page/static/js/runtime~main.bc647b25.js"
+  }
+]);
