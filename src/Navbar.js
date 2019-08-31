f8d425aa06c2ca0e07d11b5e90ab1b91b5a576ed
ZoloMohan <mohanzolomon@gmail.com>
2019-08-31 23:18:43 +0530

    Convert Navbar from Class -> Functional Component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,43 +9,43 @@ import Switch from '@material-ui/core/Switch';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavBarStyles';
 import { ThemeContext } from './contexts/ThemeContext';
-import { withLanguageContext } from './contexts/LanguageContext';
 import words from './helpers/words';
+import { LanguageContext } from './contexts/LanguageContext';
 
-class Navbar extends Component {
-	static contextType = ThemeContext;
-	render() {
-		const { darkMode, toggleTheme } = this.context;
-		const { classes, languageContext } = this.props;
-		const { search, signin, emoji } = words[languageContext.language];
-		return (
-			<div className={classes.root}>
-				<AppBar position='static' color={darkMode ? 'primary' : 'default'}>
-					<Toolbar>
-						<IconButton className={classes.menuButton} color='inherit'>
-							<span role='img'>{emoji}</span>
-						</IconButton>
-						<Typography className={classes.title} variant='h6' color='inherit'>
-							{signin}
-						</Typography>
-						<Switch onClick={toggleTheme} />
-						<div className={classes.grow} />
-						<div className={classes.search}>
-							<div className={classes.searchIcon}>
-								<SearchIcon />
-							</div>
-							<InputBase
-								placeholder={search}
-								classes={{
-									root  : classes.inputRoot,
-									input : classes.inputInput
-								}}
-							/>
+function Navbar(props) {
+	const { classes } = props;
+	const { language } = useContext(LanguageContext);
+	const { darkMode, toggleTheme } = useContext(ThemeContext);
+	const { search, signin, emoji } = words[language];
+
+	return (
+		<div className={classes.root}>
+			<AppBar position='static' color={darkMode ? 'primary' : 'default'}>
+				<Toolbar>
+					<IconButton className={classes.menuButton} color='inherit'>
+						<span role='img'>{emoji}</span>
+					</IconButton>
+					<Typography className={classes.title} variant='h6' color='inherit'>
+						{signin}
+					</Typography>
+					<Switch onClick={toggleTheme} />
+					<div className={classes.grow} />
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
 						</div>
-					</Toolbar>
-				</AppBar>
-			</div>
-		);
-	}
+						<InputBase
+							placeholder={search}
+							classes={{
+								root  : classes.inputRoot,
+								input : classes.inputInput
+							}}
+						/>
+					</div>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
-export default withLanguageContext(withStyles(styles)(Navbar));
+
+export default withStyles(styles)(Navbar);
