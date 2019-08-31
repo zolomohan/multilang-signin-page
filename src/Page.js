153cddb6a22675b65fadc36493a953c742829734
ZoloMohan <mohanzolomon@gmail.com>
2019-08-31 23:41:17 +0530

    Convert Page from Class -> Functional Component

--- src/Page.js
+++ src/Page.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
-export default class Page extends Component {
-	static contextType = ThemeContext;
-	render() {
-		console.log(this.context.darkMode)
-		return (
-			<div
-				style={{
-					backgroundColor : this.context.darkMode ? '#222' : '#ececec',
-					height          : '100vh',
-					width           : '100%'
-				}}
-			>
-				{this.props.children}
-			</div>
-		);
-	}
+
+export default function Page({ children }) {
+	const { darkMode } = useContext(ThemeContext);
+	const styles = {
+		backgroundColor : darkMode ? '#222' : '#ececec',
+		height          : '100vh',
+		width           : '100vw'
+	};
+	return <div style={styles}>{children}</div>;
 }
